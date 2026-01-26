35051f08d48696630d0438800d90b32588248d0a
Naween Jasinghe <jasinghenaween@gmail.com>
2026-01-25 19:22:42 -0500

    Add missing import for Spring Boot application

--- src/main/java/com/naw/url/UrlShortenerApplication.java
+++ src/main/java/com/naw/url/UrlShortenerApplication.java
@@ -1,6 +1,7 @@
 package com.naw.url;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class UrlShortenerApplication {
 
